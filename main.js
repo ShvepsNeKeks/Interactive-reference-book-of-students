6c325d769d943e1a98e9277d257b3c52c5b4aa6d
ShvepsNeKeks <snepe777@gmail.com>
2025-05-07 21:01:18 +0300

    add new method in Student

--- main.js
+++ main.js
@@ -3,10 +3,29 @@
 let studentData = new Map();
 let uniqueSubjects = new Set();
 
-// let student1 = new Student(1, 'Дима', ['Химия', 'Физика', 'Русский язык']);
-// studentData.set(student1.id, student1);
+let student1 = new Student(1, 'Дима', ['Химия', 'Физика', 'Русский язык']);
+let student2 = new Student(2, 'Лианна', ['Химия', 'Физика', 'Обществоезнание']);
+let student3 = new Student(3, 'Степан', ['Химия', 'Физика', 'Обществоезнание', 'Биология']);
 
-// // findStudent(3);
+student1.addUniqueSubjects();
+student2.addUniqueSubjects();
+student3.addUniqueSubjects();
+
+student1.addMap();
+student2.addMap();
+student3.addMap();
+
+// findStudent(3);
+console.log(studentData);
+console.log(uniqueSubjects);
+
+function createAuth() {
+  const passwords = new Map('admin', '1213');
+
+  return {
+    checkPassword: (login, password) => passwords.get(login) === password
+  }
+}
 
 function findStudent(id) {
   if (id == null) {
@@ -28,4 +47,8 @@ function Student(id, name, subjects) {
   this.id = id;
   this.name = name;
   this.subjects = subjects;
+  this.addMap = () => studentData.set(this.id, this);
+  this.addUniqueSubjects = () => this.subjects.forEach(elem => {
+    uniqueSubjects.add(elem)
+  });
 }
